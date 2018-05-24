8fa3d0c15ac1263acaf19ebd0e23a5f49591c0a0
sammwaura <meshsami27@gmail.com>
2018-05-24 12:40:04 +0300

    modify  CLient.java

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,108 +1,138 @@
-import java.util.ArrayList;
+
 import java.util.List;
-import org.sql2o.*;
+
+import org.sql2o.Connection;
 
 public class Client{
 
     //private variables for the client class i.e their name, phone number,stylistId and id
-    private String clientName;
-    private int phoneNumber;
+    private String name;
+    // name property
+    private String style;
+    // Style property
     private int id;
+    // id property
     private int stylistId;
-
+    
     //constructor for the Client
-    public Client(String name, int number, int stylistId){
-        this.clientName= name;
-        this.phoneNumber = number;
+    public Client(String name, String style, int stylistId){
+        this.name= name;
+
+        this.style = style;
+
         this.stylistId = stylistId;
     }
+    // method to clear Test Database
+    public static void clear(){
+        // try to make connection to test DB
+        try (Connection con = DB.sql20.open()){
 
+            String deleteClientQuery = DELETE FROM clients *;
+
+            con.createQuery(deleteClientQuery).executeUpdate();
+
+            }
+        }
+        
+        //method to get the name
+         public String getClientName() {
+
+            return name;
+         }
+        //  method to get Style of client
+        public String getClientStyle() {
+
+            return style;
+        }
+
+        // method to get all instances of Client from object
+        public static List<Client> all() {
+            String sql = "SELECT id, name, stylistId FROM clients";
+            try(Connection con = DB.sql2o.open()) {
+                return con.createQuery(sql).executeAndFetch(Client.class);
+            }
+        }
+        // method to get Stylists Id
+        public int getStylistId() {
+            return stylistId;
+        }
+        // method to get client's Id
+        public int getId() {
+
+            return id;
+        }
+        // method that saves all instances of class
+        public void save(){
+
+            try(Connection con = DB.sql20.open()) {
+
+                String sql = "INSERT INTO clients(name, style, stylistId)VALUES (:name, :style, :stylistId)";
+
+                this.id = (int) con.createQuery(sql, true)
+
+                .addParameter("name", this.name)
+
+                .addParameter("style", this.style)
+
+                .addParameter("stylistId", this.stylistId)
+
+                .executeUpdate()
+
+                .getKey();
+
+            }
+        }
+        // method to find ID
+        public static Client find(int id) {
+            try(Connection con = DB.sql2o.open()) {
+                String sql = "SELECT * FROM clients where id=:id";
+                Client client = con.createQuery(sql)
+                        .addParameter("id", id)
+                        .executeAndFetchFirst(Client.class);
+                return client;
+            }
+        }
+        // method to update info on the databse
+        public void update(String name, String style) {
+            try(Connection con = DB.sql2o.open()) {
+                String sql = "UPDATE clients SET (name, style) = (:name, :style) WHERE id= :id,";
+                con.createQuery(sql)
+                           .addParameter("name", name)
+                           .addParameter("style", style)
+                           .addParameter("id", id)
+                           .executeUpdate();
+
+            }
+        }
+        // method to delete Client from database
+        public void daleteClient(){
+            try (Connection con = DB.sql2o.open()) {
+                String sql = "DELETE FROM clients WHERE id = :id";
+                con.createQuery(sql)
+                         .addParameter("id", id)
+                         .executeUpdate();
+            }
+        }
     //method to overide the equals method
     @Override
 
     public boolean equals(Object otherClient){
+
         if (!(otherClient instanceof Client)) {
+
             return false;
+
         }else{
+
             Client newClient = (Client) otherClient;
-            return this.getName().equals(newClient.getName())&&
+
+            return this.getClientName().equals(newClient.getClientName())&&
             this.getId() == newClient.getId()&&
-            this.getStylistId() == newClient.getStylistId()&&
-            this.getPhoneNumber() == newClient.getPhoneNumber();
+            this.getStylistId() == newClient.getStylistId();
+            
 
-        }
-    }
-    //getter method for the name;
-    public String getName(){
-        return clientName;
-    }
-    //getter method for the number
-    public int getPhoneNumber(){
-        return phoneNumber;
-    }
-    //getter method for the id
-    public int getId(){
-        return id;
-    }
-    //getter method for the sylistId
-    public int getStylistId(){
-        return stylistId;
-    }
-
-    //the method gets all the data from the table clients and convets it to type object Client
-    public static List<Client> all(){
-        String sql = "SELECT * FROM clients";
-        try(Connection con = DB.sql2o.open()){
-            return con.createQuery(sql).executeAndFetch(Client.class);
-        }
-    }
-    //the method that saves the entries to the database and also gets the key id and returns it;
-    public void save(){
-        try(Connection con = DB.sql2o.open()){
-            String sql = "INSERT INTO clients(clientName, phoneNumber, stylistId) VALUES(:clientName,:phoneNumber,:stylistId)";
-            this.id = (int) con.createQuery(sql, true)
-            .addParameter( "clientName", this.clientName)
-            .addParameter("phoneNumber", this.phoneNumber)
-            .addParameter("stylistId", this.stylistId)
-            .executeUpdate()
-            .getKey();
-        }
-    }
-
-    //the find method finds the first id property in the table clients that matches the id proprty provided then casts it into a Client object
-    
-    public static Client find(int id){
-        try(Connection con = DB.sql2o.open()){
-            String sql = "SELECT * FROM clients WHERE id=:id";
-            Client client = con.createQuery(sql)
-            .addParameter("id", id)
-            .executeAndFetchFirst(Client.class);
-            return client;
-
-        }
-
-    }
-    //the method update updates the clients name and phone nummber 
-    public void update(String clientName, int clientNumber){
-        try(Connection con = DB.sql2o.open()){
-            String sql = "UPDATE clients SET clientName= :clientName, phoneNumber=:phoneNumber WHERE id=:id";
-            con.createQuery(sql)
-            .addParameter( "clientName", clientName)
-            .addParameter("phoneNumber", phoneNumber)
-            .addParameter("id",id)
-            .executeUpdate();
-        }
-
-    }
-
-    //the method  deletes the clients name and phone number 
-    public void delete(){
-        try(Connection con = DB.sql2o.open()){
-            String sql = "DELETE FROM clients WHERE id=:id";
-            con.createQuery(sql)
-            .addParameter("id", id)
-            .executeUpdate();
         }
     }
 
 }
+    
