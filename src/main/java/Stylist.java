2ec0cebbd4557560c05e4b0e19c8b981c153be25
sammwaura <meshsami27@gmail.com>
2018-05-24 15:43:16 +0300

    modify  stylist.java

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -1,108 +1,166 @@
-import org.sql2o.*;
-import java.util.ArrayList;
-import java.util.Arrays;
+import org.sql2o.Connection;
+
 import java.util.List;
+
 
 public class Stylist{
 
-    //private variables for the stylist class i.e their name, phone number and id
-    private String stylistName;
-    private int phoneNumber;
+    //private variables for the stylist class i.e their name, style and id
+    private String name;
+    // name property
+    private String style;
+    // style property
     private int id;
+    // ID property
 
+    // Constructor
+    public Stylist(String name, String style) {
+        this.name = name;
 
-    //method to overide the equals method
-    @Override
-    public boolean equals(Object otherStylist){
-        if (!(otherStylist instanceof Stylist)) {
-            return false;
-        }else{
-            Stylist newStylist = (Stylist) otherStylist;
-            return this.getName().equals(newStylist.getName())&&
-            this.getNumber()==newStylist.getNumber();
+        this.style = style;
+    }
+    // method to clear test database
+    public static void clear() {
+        // try to make a connection to test DB
+        try (Connection con = DB.sql2o.open()) {
+
+            String deleteStylistQuery = "DELETE FROM stylist *;";
+
+            con.createQuery(deleteStylistQuery).executeUpdate();
         }
-
     }
-
-    //constructor for the Stylist
-    public Stylist(String name, int number){
-        this.stylistName = name;
-        this.phoneNumber = number;
-    }
-
-    //getter method for the name;
+    // method to get the name
     public String getName(){
-        return stylistName;
+
+        return name;
     }
 
-    //getter method for the phone number;
-    public int getNumber(){
-        return phoneNumber;
+    // method to get style of stylist
+    public String getStyle() {
+
+        return style;
     }
 
-    //getter method for the id
-    public int getId(){
-        return id;
+    // method to get all inmstances of Stylist from object
+    public static List<Stylist> allStyle(){
+
+        String sql = "SELECT * FROM stylist;";
+
+        try(Connection con = DB.sql2o.open()){
+
+        return con.createQuery(sql).executeAndFetch(Stylist.class);
+
+        }
     }
-    //the method that saves the entries to the database and also gets the key id and returns it;
+    // method to save all instances of the class
     public void save(){
-        try(Connection con = DB.sql2o.open()){
-            String sql = "INSERT INTO stylists (stylistName, phoneNumber) VALUES (:stylistName, :phoneNumber)";
-            this.id = (int) con.createQuery(sql, true)
-            .addParameter( "stylistName", this.stylistName)
-            .addParameter("phoneNumber", this.phoneNumber)
+
+        try(Connection con = DB.sql2o.open()) {
+
+            String sql = "INSERT INTO stylist (name, style)VALUES(:name, :style)";
+
+            this.id = (int) con.createQuery(sql,true)
+
+            .addParameter("name", this.name)
+
+            .addParameter("style", this.style)
+
             .executeUpdate()
+
             .getKey();
-        }
-    }
-    //the method gets all the data from the table stylist and convets it to type object Stylist
-    public static List<Stylist>all(){
-        String sql = "SELECT * FROM stylists";
-        try(Connection con = DB.sql2o.open()){
-            return con.createQuery(sql).executeAndFetch(Stylist.class);
-        }
-    }
-    //the find method finds the first id property in the table stylist that matches the id proprty provided then casts it into a Stylist object
-    public static Stylist find(int id){
-        try(Connection con = DB.sql2o.open()){
-        String sql = "SELECT * FROM stylists WHERE id=:id";
-        Stylist mystylist = con.createQuery(sql)
-        .addParameter("id", id)
-        .executeAndFetchFirst(Stylist.class);
-        return mystylist;
-    }
-    }
 
-    //the method  updates the stylist name and phone number 
-    public void update(String stylistName, int phoneNumber){
-        try(Connection con = DB.sql2o.open()){
-            String sql = "UPDATE stylists SET stylistName= :stylistName, phoneNumber= :phoneNumber WHERE id= :id";
-            con.createQuery(sql)
-            .addParameter( "stylistName", stylistName)
-            .addParameter("phoneNumber", phoneNumber)
-            .addParameter("id", id)
-            .executeUpdate();
-        }
-
-    }
-    //the method  deletes the stylist name and phone number 
-
-    public void delete(){
-        try(Connection con = DB.sql2o.open()){
-            String sql = "DELETE FROM stylists WHERE id=:id";
-            con.createQuery(sql)
-            .addParameter("id", id)
-            .executeUpdate();
         }
     }
 
-    //method that retrieves all the clients that are saved under a specific Stylist
-    public List<Client> getClients(){
-        try(Connection con = DB.sql2o.open()){
-            String sql="SELECT * FROM clients WHERE stylistid=:id";
+    // method to get ID of a stylist
+    public int getId() {
+        return id;
+
+    }
+
+    // method to find ID
+    public static Stylist find(int id) {
+        try(Connection con = DB.sql2o.open()) {
+            String stylist = con.createQuery(sql)
+                .addParameter("id", id)
+                .executeAndFetchFirst(Stylist.class);
+            return stylist;
+        }
+    }
+    // method to retrieve Clients
+    public List<Client> getClients() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM clients where stylistid=:id";
             return con.createQuery(sql)
             .addParameter("id", this.id)
             .executeAndFetch(Client.class);
         }
     }
+    // method to update info on database
+    public void update(String name, String style) {
+
+        try(Connection con = DB.sql2o.open()) {
+
+            String sql = "UPDATE stylist SET (name, style) = (:name, :style) WHERE id = :id;";
+
+            con.createQuery(sql)
+
+            .addParameter("name", name)
+
+            .addParameter("style", style)
+
+            .addParameter("id", this.id)
+
+            .executeUpdate();
+
+        }
+    }
+
+    // method to delete Stylist from Database
+    public void delete(){
+        try(Connection con = DB.sql2o.open()) {
+
+            String sql = "DELETE FROM stylist WHERE id = :id";
+
+            con.createQuery(sql)
+
+                    .addParameter("id", this.id)
+
+                    .executeUpdate();
+
+        }
+    }
+
+    // method to get all the clients from Database
+    public List<Client> all() {
+
+        String sql = "SELECT * FROM clientys WHERE id = :id";
+
+        try(Connection con = DB.sql2o.open()) {
+
+            return con.createQuery(sql)
+
+                       .addParameter("id", this.id)
+                       
+                       .executeAndFetch(Client.class);
+        }
+    }
+    //method to overide the equals method
+    @Override
+    public boolean equals(Object otherStylist){
+
+        if (!(otherStylist instanceof Stylist)) {
+
+            return false;
+
+        }else{
+
+            Stylist newStylist = (Stylist) otherStylist;
+
+            return this.getName().equals(newStylist.getName());
+        }
+
+    }
 }
+
+   
